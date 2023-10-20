8579fd392feb2c0263ce672fc73e17fc890ddac5
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-10-20 09:34:44 +0200

    New function `matrix_indices()` that returns matrix indices as character.

--- NEWS.md
+++ NEWS.md
@@ -4,6 +4,8 @@
 
 * New function `stationary_distribution()` that computes the stationary distribution corresponding to a transition probability matrix.
 
+* New function `matrix_indices()` that returns matrix indices as character.
+
 # oeli 0.1.0
 
 * Initial CRAN submission.
--- R/matrix_helpers.R
+++ R/matrix_helpers.R
@@ -1,5 +1,4 @@
-# These functions allows for more informative or abbreviated printing of
-# matrix-type objects.
+# These functions help when working with matrix-type objects.
 
 #' Print (abbreviated) \code{matrix}
 #'
@@ -149,3 +148,42 @@ print_matrix <- function(
   }
   return(invisible(x))
 }
+
+#' Get \code{matrix} indices
+#'
+#' @description
+#' This function returns \code{matrix} indices as \code{character}.
+#'
+#' @param x
+#' A \code{matrix}.
+#' @param prefix
+#' A \code{character} as prefix for the indices.
+#' @param exclude_diagonal
+#' Either \code{TRUE} to exclude indices where row equals column, or
+#' \code{FALSE} to include those.
+#'
+#' @return
+#' A \code{character} \code{vector}.
+#'
+#' @examples
+#' M <- diag(3)
+#' matrix_indices(M)
+#' matrix_indices(M, "M_")
+#' matrix_indices(M, "M_", TRUE)
+#'
+#' @export
+
+matrix_indices <- function(x, prefix = "", exclude_diagonal = FALSE) {
+  checkmate::assert_matrix(x)
+  checkmate::assert_string(prefix)
+  checkmate::assert_flag(exclude_diagonal)
+  rows <- nrow(x)
+  cols <- ncol(x)
+  indices <- expand.grid(seq_len(rows), seq_len(cols))
+  if (exclude_diagonal) {
+    indices <- indices[apply(indices, 1, function(x) length(unique(x)) != 1), ]
+  }
+  indices <- apply(indices, 1, paste, collapse = "")
+  paste0(prefix, indices)
+}
+
