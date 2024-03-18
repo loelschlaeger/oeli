d402853c23502f5cbf4e3c1ea6796a34a12feb6a
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2024-03-18 11:10:51 +0100

    `insert_matrix_column()` now also for single value and at multiple positions

--- R/insert_matrix_column.R
+++ R/insert_matrix_column.R
@@ -7,8 +7,9 @@
 #' A \code{matrix}.
 #'
 #' @param x
-#' A \code{numeric} \code{vector} of length \code{nrow(A)}, the column to be
-#' added.
+#' A \code{vector} of length \code{nrow(A)}, the column to be added.
+#'
+#' Can also be a single value.
 #'
 #' @param p
 #' An \code{integer}, the position where to add the column:
@@ -17,6 +18,8 @@
 #' - \code{p = n} inserts the column between the \code{n}-th and
 #'   \code{(n + 1)}-th column of \code{A}.
 #'
+#' Can also be a \code{vector} of multiple positions.
+#'
 #' @return
 #' A \code{matrix}.
 #'
@@ -24,23 +27,43 @@
 #'
 #' @examples
 #' A <- diag(3)
-#' x <- numeric(3)
+#' x <- 1:3
 #' insert_matrix_column(A, x, 0)
 #' insert_matrix_column(A, x, 1)
 #' insert_matrix_column(A, x, 2)
 #' insert_matrix_column(A, x, 3)
+#'
+#' ### also single value
+#' x <- 2
+#' insert_matrix_column(A, x, 0)
+#'
+#' ### also multiple positions
+#' insert_matrix_column(A, x, 0:3)
 
 insert_matrix_column <- function(A, x, p) {
-  checkmate::assert_matrix(A, mode = "numeric", min.rows = 1, min.cols = 1)
+  checkmate::assert_matrix(A, min.rows = 1, min.cols = 1)
   n <- ncol(A)
-  m <- nrow(A)
-  assert_numeric_vector(x, len = m)
-  checkmate::assert_int(p, lower = 0, upper = n)
-  if (p == 0) {
-    cbind(x, A, deparse.level = 0)
-  } else if (p == n) {
-    cbind(A, x, deparse.level = 0)
-  } else {
-    cbind(A[, seq_len(p)], x, A[, (p + 1):n], deparse.level = 0)
+  checkmate::assert_atomic_vector(x, min.len = 1)
+  stopifnot(
+    "'x' must be of length 1 or nrow(A)" = length(x) == 1 || length(x) == nrow(A)
+  )
+  checkmate::assert_integerish(
+    p, lower = 0, upper = n, unique = TRUE, min.len = 1, max.len = n + 1
+  )
+  p <- sort(p)
+  for (i in seq_along(p)) {
+    if (p[i] == 0) {
+      A <- cbind(x, A, deparse.level = 0)
+    } else if (p[i] == n) {
+      A <- cbind(A, x, deparse.level = 0)
+    } else {
+      A <- cbind(
+        A[, seq_len(p[i]), drop = FALSE], x, A[, (p[i] + 1):n, drop = FALSE],
+        deparse.level = 0
+      )
+    }
+    p <- p + 1
+    n <- ncol(A)
   }
+  return(A)
 }
