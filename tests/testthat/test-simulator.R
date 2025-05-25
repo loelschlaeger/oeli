de15607a6cdcc2ad9b41e16be7c8e08248d246e8
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2025-05-25 21:15:15 +0200

    comment-out test for now

--- tests/testthat/test-simulator.R
+++ tests/testthat/test-simulator.R
@@ -1,28 +1,27 @@
-
-### define progress bar and parallel computation
-progressr::handlers(global = TRUE)
-progressr::handlers(
-  progressr::handler_progress(format = ">> :percent, :eta to go :message")
-)
-future::plan(future::multisession, workers = 2)
-
-f <- function() { Sys.sleep(runif(1)); rnorm(1, sd = 0.1) }
-
-g <- function(x, y) { Sys.sleep(runif(1)); x + y + rnorm(1, sd = 0.1) }
-
-h <- function(x, y = 1) { Sys.sleep(runif(1)); x + y + rnorm(1, sd = 0.1) }
-
-
-
-
-sim <- Simulator$new(verbose = TRUE)
-
-self <- sim
-private <- self$.__enclos_env__$private
-
-sim$define(f = g, x = as.list(1:2), y = as.list(1:2))
-
-sim$go(runs = 2)
-
-sim$results
-
+#
+# ### define progress bar and parallel computation
+# progressr::handlers(global = TRUE)
+# progressr::handlers(
+#   progressr::handler_progress(format = ">> :percent, :eta to go :message")
+# )
+# future::plan(future::multisession, workers = 2)
+#
+# f <- function() { Sys.sleep(runif(1)); rnorm(1, sd = 0.1) }
+#
+# g <- function(x, y) { Sys.sleep(runif(1)); x + y + rnorm(1, sd = 0.1) }
+#
+# h <- function(x, y = 1) { Sys.sleep(runif(1)); x + y + rnorm(1, sd = 0.1) }
+#
+#
+#
+# sim <- Simulator$new(verbose = TRUE)
+#
+# self <- sim
+# private <- self$.__enclos_env__$private
+#
+# sim$define(f = g, x = as.list(1:2), y = as.list(1:2))
+#
+# sim$go(runs = 2)
+#
+# sim$results
+#
